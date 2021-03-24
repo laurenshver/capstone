652acc43d572e29547dc8db8805214ffbec8e614
Lauren S <lauren_shver@hotmail.com>
2021-03-23 21:14:00 -0400

    update patron and business

--- customer/templatetags/string_multiply.py
+++ customer/templatetags/string_multiply.py
@@ -1,5 +1,5 @@
 from django.template import Library
-
+from customer.models import PatronPhoneNumbers, BusinessContact
 register = Library()
 
 @register.filter
@@ -11,3 +11,22 @@ def multiply(string, times):
 def unchecked(string, times):
     times = int(times)
     return string  * (5-times)
+
+@register.filter
+def get_num(patron):
+    x = PatronPhoneNumbers.objects.get(PatronID_id = patron, PhoneNumberID_id__PhoneNumberType = 2)
+    return x.PhoneNumberID.PhoneNumber
+
+@register.filter
+def primarycontact(business):
+    return BusinessContact.objects.get(BusinessID_id = business, PrimaryContact = "Y")
+
+@register.filter
+def primarycontact_number(business):
+    x =  BusinessContact.objects.get(BusinessID_id = business, PrimaryContact = "Y")
+    return x.PhoneNumberID.PhoneNumber
+
+@register.filter
+def primarycontact_role(business):
+    x =  BusinessContact.objects.get(BusinessID_id = business, PrimaryContact = "Y")
+    return x.BusinessContactRoleID.BusinessContactRole
